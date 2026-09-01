466c7aff4c7308cdc38a7df1bd2578c14c2a030a
Ayush <ayushbhor04@gmail.com>
2026-09-01 19:42:16 +0530

    Day 1 final commit

--- Day-01-Input-Statements.py
+++ Day-01-Input-Statements.py
@@ -3,40 +3,85 @@
 
 # Question 1:
 # Take the user's name as input and print a greeting message using their name.
+# name=input("Enter your name : ")
+# print("Welcome to Python",name) 
 
 # Question 2:
 # Take the user's name and age as input and print both values.
+# name=input("Enter your name : ")
+# age=int(input("Enter your age : "))
+# print("Welcome",name,"your age is",age)
 
 # Question 3:
 # Take two numbers as input and print them.
+# a=int(input("Enter any number : "))
+# b=int(input("Enter any number : "))
+# print("Your 2 numbers are",a,b)
 
 # Question 4:
 # Take the user's name, city, and country as input and display them.
+# name=input("Enter your name : ")
+# city=input("Enter your city : ")
+# country=input("Enter your country : ")
+# print("There is a user named",name,"from",country,"who resides in",city)
 
 # Question 5:
 # Take a student's name, roll number, and course as input and display the details.
+# name=input("Enter your name : ")
+# roll_number=input("Enter your city : ")
+# course=input("Enter your course : ")
+# print("Name of the student is",name,"with roll number",roll_number,"studying",course)
 
 # 🟡 Medium
 
 # Question 6:
 # Take the user's name, age, height, and weight as input. Display all the information with appropriate labels.
+# name=input("Enter your name : ")
+# age=int(input("Enter your age : "))
+# height=int(input("Enter your height : "))
+# weight=int(input("Enter your weight : "))
+# print("PERSONAL DETAILS")
+# print("Name : ",name)
+# print("Age : ",age)
+# print("Height : ",height)
+# print("Weight : ",weight)
 
 # Question 7:
 # Take a product's name, price, and quantity as input. Display the product details.
+# name=input("Enter product name : ")
+# company=input("Enter prducts company name : ")
+# price=int(input("Enter product price : "))
+# print("PRODUCT DETAILS")
+# print("Product name : ",name)
+# print("Company name : ",company)
+# print("Price : ",price)
 
 # Question 8:
 # Take marks for three subjects — Python, Mathematics, and Database — as input and display the marks.
+# python=int(input("Enter Python marks : "))
+# maths=int(input("Enter Maths marks : "))
+# database=int(input("Enter Database marks : "))
+# print("STUDENT MARKS")
+# print("Python",python)
+# print("Maths",maths)
+# print("Database",database)
 
 # Question 9:
 # Take the user's first name and last name as separate inputs and display their full name.
+# first=input("First name : ")
+# last=input("Last name : ")
+# print("Full name : ",first+last) 
 
 # Question 10:
 # Take the user's name, age, college, course, and semester as input and display all five details neatly.
-
-# 🔴 Hard
-
-# Question 11:
-# Take three numbers from the user in a single line, separated by spaces, and store them in three separate variables.
-
-# Question 12:
-# Take the user's name, age, and height in a single line, separated by spaces. Store each value in the appropriate data type and display them separately.
+# name=input(("Enter your name : "))
+# age=input(("Enter your age : "))
+# college=input(("Enter your college : "))
+# course=input(("Enter your course : "))
+# semester=input(("Enter your semester : "))
+# print("STUDENT DETAILS")
+# print("Name : ",name)
+# print("Age : ",age)
+# print("College : ",college)
+# print("Course : ",course)
+# print("Semester : ",semester)
