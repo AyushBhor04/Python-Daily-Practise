1f981b7b127c0a8b8dcfe940ebfbed168e9a9c2f
Ayush <ayushbhor04@gmail.com>
2026-09-02 20:20:01 +0530

    Day 2 final commit

--- Day-02-Basic-Operations.py
+++ Day-02-Basic-Operations.py
@@ -9,40 +9,77 @@
 
 # Question 2:
 # Take two numbers as input and print their difference.
+# a=int(input("Enter first number :"))
+# b=int(input("Enter second number :"))
+# print("Difference of the two numbers is :",a-b)
 
 # Question 3:
 # Take two numbers as input and print their product.
+# a=int(input("Enter first number :"))
+# b=int(input("Enter second number :"))
+# print("Product of two numbers is :",a*b)
 
 # Question 4:
 # Take two numbers as input and print their division result.
+# a=int(input("Enter first number :"))
+# b=int(input("Enter second number :"))
+# print("Difference of the two numbers is :",a/b)
 
 # Question 5:
 # Take a number as input and print its square and cube.
+# num=int(input("Enter any number : "))
+# print("Square of the number is ",num**2)
+# print("Cube of the number is ",num**3)
 
 # 🟡 Medium
 
 # Question 6:
 # Take the length and width of a rectangle as input and calculate its area.
+# length=int(input("Enter length of the rectangle :"))
+# width=int(input("Enter length of the rectangle :"))
+# print("Area of the rectangle is :",length*width)
 
 # Question 7:
 # Take the length and width of a rectangle as input and calculate its perimeter.
+# length=int(input("Enter length of the rectangle :"))
+# width=int(input("Enter length of the rectangle :"))
+# print("Perimeter of the rectangle is :",2*(length+width))
 
 # Question 8:
 # Take the radius of a circle as input and calculate its area.
 # Use π = 3.14.
+# pi=3.14
+# radius=int(input("Enter the radius of the circle : "))
+# print("Area of the circle : ",pi*radius*radius)
 
 # Question 9:
 # Take the marks of three subjects as input and calculate the student's total marks and average marks.
+# p=int(input("Enter marks for physics : "))
+# c=int(input("Enter marks for chemistry : "))
+# m=int(input("Enter marks for maths : "))
+# print("Total marks = ",p+c+m)
+# print("Average marks = ",(p+c+m)/3)
 
 # Question 10:
 # Take a number as input and calculate its remainder when divided by 2, 3, and 5.
+# num=int(input("Enter any number : "))
+# print("When divided by 2 , remainder = ",num%2)
+# print("When divided by 3 , remainder = ",num%3)
+# print("When divided by 5 , remainder = ",num%5)
 
 # Question 11:
 # Take the price of a product and quantity purchased as input. Calculate the total cost.
+# p_1=int(input("Enter the price of product 1 : "))
+# p_2=int(input("Enter the price of product 2 : "))
+# p_3=int(input("Enter the price of product 3 : "))
+# q_1=int(input("Enter the quantity for product 1 : "))
+# q_2=int(input("Enter the quantity for product 2 : "))
+# q_3=int(input("Enter the quantity for product 3 : "))
+# total=(p_1*q_1)+(p_2*q_2)+(p_3*q_3)
+# print("Total bill for your purchase : ",total) 
 
 # Question 12:
 # Take the temperature in Celsius as input and convert it to Fahrenheit.
-
-# Formula:
-
-# F = (C × 9/5) + 32
+# Formula: F = (C × 9/5) + 32
+# c=int(input("Enter temperature in celsuis : "))
+# print("Temperature in Farhenheit is : ",(c*(9/5)+32))
