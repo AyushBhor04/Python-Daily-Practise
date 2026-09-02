de9349456c03be9ef0945f2812c7711450f4aea1
Ayush <ayushbhor04@gmail.com>
2026-09-02 19:53:21 +0530

    Day-2 initial commit

---
+++ Day-02-Basic-Operations.py
@@ -0,0 +1,48 @@
+# Day 2 — Basic Operations
+# 🟢 Easy
+
+# Question 1:
+# Take two numbers as input and print their sum.
+# a=int(input("Enter first number :"))
+# b=int(input("Enter second number :"))
+# print("Sum of the two numbers is :",a+b)
+
+# Question 2:
+# Take two numbers as input and print their difference.
+
+# Question 3:
+# Take two numbers as input and print their product.
+
+# Question 4:
+# Take two numbers as input and print their division result.
+
+# Question 5:
+# Take a number as input and print its square and cube.
+
+# 🟡 Medium
+
+# Question 6:
+# Take the length and width of a rectangle as input and calculate its area.
+
+# Question 7:
+# Take the length and width of a rectangle as input and calculate its perimeter.
+
+# Question 8:
+# Take the radius of a circle as input and calculate its area.
+# Use π = 3.14.
+
+# Question 9:
+# Take the marks of three subjects as input and calculate the student's total marks and average marks.
+
+# Question 10:
+# Take a number as input and calculate its remainder when divided by 2, 3, and 5.
+
+# Question 11:
+# Take the price of a product and quantity purchased as input. Calculate the total cost.
+
+# Question 12:
+# Take the temperature in Celsius as input and convert it to Fahrenheit.
+
+# Formula:
+
+# F = (C × 9/5) + 32
