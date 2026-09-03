8a661fbc5fe0766995ef35b579a7fd577a25a75e
Ayush <ayushbhor04@gmail.com>
2026-09-03 23:14:26 +0530

    Day 3 initail commit

---
+++ Day-03-Conditional-Statements.py
@@ -0,0 +1,60 @@
+# Day 3 — Conditional Statements
+# 🟢 Easy
+
+# Question 1:
+# Take a number as input and check whether it is positive or negative.
+# num=int(input("Enter any number : "))
+# if(num<0):
+#     print(num,"is negative")
+# else:
+#     print(num,"is positive")
+
+# Question 2:
+# Take a number as input and check whether it is even or odd.
+
+# Question 3:
+# Take a person's age as input and check whether they are eligible to vote.
+# Voting age: 18.
+
+# Question 4:
+# Take two numbers as input and print which number is greater.
+
+# Question 5:
+# Take a number as input and check whether it is zero or not.
+
+# 🟡 Medium
+
+# Question 6:
+# Take three numbers as input and print the greatest number.
+
+# Question 7:
+# Take a student's marks as input and print:
+
+# "Pass" if marks are 40 or above
+# "Fail" if marks are below 40
+
+# Question 8:
+# Take marks as input and print the grade:
+
+# 90–100 → A
+# 80–89 → B
+# 70–79 → C
+# 60–69 → D
+# Below 60 → F
+
+# Question 9:
+# Take a number as input and check whether it is divisible by both 5 and 10.
+
+# Question 10:
+# Take a person's age as input and classify them as:
+
+# Below 13 → Child
+# 13–19 → Teenager
+# 20–59 → Adult
+# 60 or above → Senior Citizen
+
+# Question 11:
+# Take three numbers as input and print the smallest number.
+
+# Question 12:
+# Take a year as input and check whether it is a leap year or not.
