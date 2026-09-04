0936b4862f2c725a4fb0046940fe427f6201f33f
Ayush <ayushbhor04@gmail.com>
2026-09-04 18:46:48 +0530

    Day 4 initial commit

---
+++ Day-04-Strings.py
@@ -0,0 +1,40 @@
+# Day 4 — Strings
+# 🟢 Easy
+
+# Question 1:
+# Take a string as input and print its length.
+
+# Question 2:
+# Take the user's name as input and print it in uppercase.
+
+# Question 3:
+# Take the user's name as input and print it in lowercase.
+
+# Question 4:
+# Take a string as input and print its first character and last character.
+
+# Question 5:
+# Take a string as input and count how many times the letter "a" appears in it.
+
+# 🟡 Medium
+
+# Question 6:
+# Take a string as input and check whether it starts with "A".
+
+# Question 7:
+# Take a string as input and check whether it ends with "ing".
+
+# Question 8:
+# Take a sentence as input and count the number of spaces in it.
+
+# Question 9:
+# Take a string as input and reverse it.
+
+# Question 10:
+# Take a string as input and check whether it is a palindrome.
+
+# Question 11:
+# Take the user's full name as input and print the number of characters in the name, excluding spaces.
+
+# Question 12:
+# Take a sentence as input and replace every space " " with a hyphen "-".
