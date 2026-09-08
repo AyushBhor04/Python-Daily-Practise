5ea92554877d10754afd3c0d9f7de4664cc4e300
Ayush <ayushbhor04@gmail.com>
2026-09-08 17:57:10 +0530

    Day 5 initial commit

---
+++ Day-05-For-Loop.py
@@ -0,0 +1,48 @@
+# Day 5 — for Loop
+# 🟢 Easy
+
+# Question 1:
+# Print the numbers from 1 to 10 using a for loop.
+
+# Question 2:
+# Take a number n as input and print the numbers from 1 to n.
+
+# Question 3:
+# Print all even numbers from 1 to 20 using a for loop.
+
+# Question 4:
+# Print all odd numbers from 1 to 20 using a for loop.
+
+# Question 5:
+# Take a number as input and print its multiplication table from 1 to 10.
+
+# 🟡 Medium
+
+# Question 6:
+# Take a number n as input and calculate the sum of all numbers from 1 to n.
+
+# Question 7:
+# Take a number n as input and calculate the sum of all even numbers from 1 to n.
+
+# Question 8:
+# Take a number n as input and print all numbers between 1 and n that are divisible by 3.
+
+# Question 9:
+# Take a string as input and print each character on a separate line using a for loop.
+
+# Question 10:
+# Take a string as input and count the number of vowels (a, e, i, o, u) in it using a for loop.
+
+# Question 11:
+# Take a number as input and calculate its factorial using a for loop.
+
+# Question 12:
+# Take a number n as input and print the following pattern:
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# For example, if n = 5, print 5 rows
