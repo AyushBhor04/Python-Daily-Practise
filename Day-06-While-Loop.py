58ca66beb8788085808dda424a0e278c72f98d7e
Ayush <ayushbhor04@gmail.com>
2026-09-09 21:41:19 +0530

    Day 6 initial update

---
+++ Day-06-While-Loop.py
@@ -0,0 +1,40 @@
+# Day 6 — while Loop
+# 🟢 Easy
+
+# Question 1:
+# Print the numbers from 1 to 10 using a while loop.
+
+# Question 2:
+# Take a number n as input and print the numbers from 1 to n.
+
+# Question 3:
+# Print all even numbers from 1 to 20 using a while loop.
+
+# Question 4:
+# Print all odd numbers from 1 to 20 using a while loop.
+
+# Question 5:
+# Take a number as input and print its multiplication table from 1 to 10.
+
+# 🟡 Medium
+
+# Question 6:
+# Take a number n as input and calculate the sum of all numbers from 1 to n.
+
+# Question 7:
+# Take a number n as input and calculate the sum of all even numbers from 1 to n.
+
+# Question 8:
+# Take a number as input and print its digits one by one using a while loop.
+
+# Question 9:
+# Take a number as input and count how many digits it contains.
+
+# Question 10:
+# Take a number as input and calculate the sum of its digits.
+
+# Question 11:
+# Take a number as input and reverse the number using a while loop.
+
+# Question 12:
+# Take a number as input and check whether it is a palindrome number.
