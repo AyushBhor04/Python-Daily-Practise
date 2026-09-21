d88e4cd475188a2643c8ad3b3c65e8ea3ba92ee0
Ayush <ayushbhor04@gmail.com>
2026-09-21 15:02:49 +0530

    Day 8 initial commit

---
+++ Day-08-Tuples.py
@@ -0,0 +1,40 @@
+# Day 8 — Tuples
+# 🟢 Easy
+
+# Question 1:
+# Create a tuple containing 5 numbers and print the tuple.
+
+# Question 2:
+# Create a tuple containing different data types and print each element.
+
+# Question 3:
+# Take a tuple and print its first and last elements.
+
+# Question 4:
+# Take a tuple as input and find its length.
+
+# Question 5:
+# Take a tuple of numbers and print each element using a for loop.
+
+# 🟡 Medium
+
+# Question 6:
+# Take a tuple of numbers and calculate the sum of all its elements.
+
+# Question 7:
+# Take a tuple of numbers and find the largest and smallest elements.
+
+# Question 8:
+# Take a tuple and search for an element entered by the user. Print whether it exists or not.
+
+# Question 9:
+# Take a tuple containing repeated elements and count how many times a particular element appears.
+
+# Question 10:
+# Take a tuple of numbers and create a new tuple containing only the even numbers.
+
+# Question 11:
+# Take a tuple and reverse it using slicing.
+
+# Question 12:
+# Take a tuple containing numbers and convert it into a list. Then add a new number to the list and convert it back into a tuple.
